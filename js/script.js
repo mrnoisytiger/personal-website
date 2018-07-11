5a541d354ea6dd2bd0277b392f8e32417dd66a88
Felix Jen <fjen@felixjen.com>
2018-07-11 14:08:42 -0700

    Added scrolling to videos

--- js/script.js
+++ js/script.js
@@ -14,7 +14,13 @@ $(document).ready(function() {
 
     $('#splash-scroll-pictures').click(function() {
         $('body').velocity({
-            scrollTop: ($('#pictures').offset().top)
+            scrollTop: ($('#pictures').offset().top - 100)
+        },500);
+    });
+
+    $('#splash-scroll-videos').click(function() {
+        $('body').velocity({
+            scrollTop: ($('#videos').offset().top - 100)
         },500);
     });
 });
