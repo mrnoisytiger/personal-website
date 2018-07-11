1403620814e775495d0d82043c46d766ff837cc2
Felix Jen <fjen@felixjen.com>
2018-07-11 15:06:08 -0700

    Changed submission animations

--- js/form.js
+++ js/form.js
@@ -1,7 +1,5 @@
 function ani(){
     $("#plane").fadeOut(100);
     document.getElementById('plane').className ='animation';
-}
-function anitwo(){
     document.getElementById('bg').className ='animation2';
 }
