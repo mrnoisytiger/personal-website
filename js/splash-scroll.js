52b6b31128268b97f0103ae849b817c199bc70a4
Felix Jen <fjen@felixjen.com>
2021-04-20 21:32:37 -0500

    added html to body scroll

--- js/splash-scroll.js
+++ js/splash-scroll.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $('#splash-scroll-icon').click(function() {
-        $('body').animate({
+        $('html, body').animate({
             scrollTop: ($('#about').offset().top)
         },500);
     });
 
     $('#splash-scroll-pictures').click(function() {
-        $('body').animate({
+        $('html, body').animate({
             scrollTop: ($('#pictures').offset().top - 100)
         },500);
     });
 
     $('#splash-scroll-videos').click(function() {
-        $('body').animate({
+        $('html, body').animate({
             scrollTop: ($('#videos').offset().top - 100)
         },500);
     });
